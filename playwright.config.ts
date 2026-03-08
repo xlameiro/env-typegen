ccdf34c281950e217acedbc9bef636ccbea595de
xabierlameiro <xabier.lameiro@gmail.com>
2026-03-08 19:52:03 +0100

    feat: more rules

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, devices } from "@playwright/test";
 export default defineConfig({
   testDir: "./tests",
   fullyParallel: true,
-  forbidOnly: !!process.env.CI,
+  forbidOnly: Boolean(process.env.CI),
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: [["html"], process.env.CI ? ["github"] : ["list"]],
